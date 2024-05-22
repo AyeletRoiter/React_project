625d17a8301a509a9e941f0a07fc0faf80a60d2b
AyeletRoiter <roiterschool@gmail.com>
2024-05-22 14:44:14 +0300

    I arranged the sessions that would really display the objects from the List and changed the language to English

--- React1/src/Classes/Meeting_Class.jsx
+++ React1/src/Classes/Meeting_Class.jsx
@@ -3,7 +3,18 @@ import { makeObservable, observable, action } from 'mobx';
 const baseUrl = 'http://localhost:8787';
 
 class Meeting {
-  MeetingList = [];
+  MeetingList = [
+    {
+      NameClient: "Ayelet",
+      MeetDescription: "a meeting for information",
+      Date: new Date("2024-04-25")
+    },
+    {
+      NameClient: "Hadass",
+      MeetDescription: "a meeting for information",
+      Date: new Date("2024-04-22")
+    }
+  ];
 
   constructor() {
     makeObservable(this, {
@@ -14,9 +25,15 @@ class Meeting {
 
   async addMeeting(newMeet) {
     try {
+      // Convert Date object to ISO string for sending to the server
+      const meetingToSend = {
+        ...newMeet,
+        Date: newMeet.Date.toISOString()
+      };
+
       const res = await fetch(`${baseUrl}/appointment`, {
         method: 'POST',
-        body: JSON.stringify(newMeet),
+        body: JSON.stringify(meetingToSend),
         headers: {
           'Content-Type': 'application/json',
           'Accept': 'application/json'
@@ -26,6 +43,8 @@ class Meeting {
       if (res.status === 200) {
         console.log("Meeting added successfully!");
         const newData = await res.json();
+        // Convert received date string back to Date object
+        newData.Date = new Date(newData.Date);
         this.MeetingList.push(newData);
         console.log(this.MeetingList);
       } else {
--- React1/src/Meeting.jsx
+++ React1/src/Meeting.jsx
@@ -1,22 +1,19 @@
 import Meeting_Class from './Classes/Meeting_Class';
-import {observer } from 'mobx-react-lite';
+import { observer } from 'mobx-react-lite';
 
-
- const MeetingShow = observer(()=>{
-    const Meetinglist = Meeting_Class.MeetingList;
-    return(
+const MeetingShow = observer(() => {
+  const Meetinglist = Meeting_Class.MeetingList;
+  return (
     <div>
-        {Meetinglist.map((meet)=>(
-        <div style={{ border: '1px solid'}}>
-             <h5> שם הלקוח: {meet.NameClient}</h5>
-            <p>{meet.MeetDescription}</p>
-          <p>תאריך: {meet.Date}</p>
-          
-          </div>
-           
+      {Meetinglist.map((meet) => (
+        <div key={meet.NameClient + meet.Date} style={{ border: '1px solid' }}>
+          <h5>User Name: {meet.NameClient}</h5>
+          <p>Description: {meet.MeetDescription}</p>
+          <p>Date: {meet.Date.toLocaleDateString()}</p>
+        </div>
       ))}
     </div>
-  )
-})
+  );
+});
 
 export default MeetingShow;
